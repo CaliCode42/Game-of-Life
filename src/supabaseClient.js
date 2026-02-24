c1a2e25fc2dbbe0a398585101b4396f72cc8be2f
CaliCode42 <tcali@student.42.fr>
2026-02-24 13:47:12 +0100

    Use env vars for Supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://luzcuwmashnvvolopjdj.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1emN1d21hc2hudnZvbG9wamRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5MjE2NzIsImV4cCI6MjA4NzQ5NzY3Mn0.4ZvbDric3K5jwvFivgHD6xKGLM1t_sIrBSI-kGApXtY"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://luzcuwmashnvvolopjdj.supabase.co"
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1emN1d21hc2hudnZvbG9wamRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5MjE2NzIsImV4cCI6MjA4NzQ5NzY3Mn0.4ZvbDric3K5jwvFivgHD6xKGLM1t_sIrBSI-kGApXtY"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
